a3a36a0c72df9893bf9913acbdc48e33f736714c
Mizuho Okimoto <mokimoto@myseneca.ca>
2022-09-25 16:07:02 -0400

    Add footer part

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,65 @@
+import React from 'react';
 import styled from "styled-components";
+import './css/Footer.css';
+//import { Link } from "react-router-dom";
 
 const FooterBox = styled.footer`
   bottom: 0;
   width: 100%;
+  background-color: antiquewhite;
 `;
 
 const Footer = () => {
   return (
     <FooterBox>
       <div className="footer-box">
-        <hr />
-        <p className="copyright">
-          Copyright ©{new Date().getFullYear()} All rights reserved | This site
-          is made by Jun Song
-        </p>
+        
+        <section className="footer">
+    
+      <section className="footer-info">
+        <section className="footer-info-left">
+          <section className="footer-info__title">
+              StudentFair
+          </section>
+          <section to="/" className="footer-info__contents">
+              About us
+          </section>
+          <section className="footer-info__contents">
+            <a href="https://github.com/MizuhoOkimoto/StudentFair" target="blank">
+              GitHub
+            </a>
+          </section>       
+        </section>
+
+        <section className="footer-info-center">
+          <section className="footer-info__title">
+            EXPLORE
+          </section>
+          <section className="footer-info__contents">
+            Home
+          </section>
+          <section className="footer-info__contents">
+            Product list
+          </section>
+        </section>
+        <section className="footer-info-right">
+          <section className="footer-info__title">
+            SUPPORT
+          </section>
+          <section className="footer-info__contents">
+            Contact us
+          </section>
+          <section className="footer-info__contents">
+            Report post
+          </section>
+        </section>
+      </section>
+
+      <section className="copy-right">
+      Copyright ©{new Date().getFullYear()} All rights reserved | PRJ666 Team 9
+          </section>
+
+    </section>
       </div>
     </FooterBox>
   );
