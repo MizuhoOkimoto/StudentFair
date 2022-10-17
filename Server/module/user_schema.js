d96d880537ce93c91e83a9570208f90043a6d5dd
Mizuho Okimoto <mokimoto@myseneca.ca>
2022-10-17 17:13:52 -0400

    Delete birthday

--- Server/module/user_schema.js
+++ Server/module/user_schema.js
@@ -8,7 +8,7 @@ const userSchema = new Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
+      unique: true, //validation
       trim: true,
       minlength: 3,
     },
--- Server/routes/users.js
+++ Server/routes/users.js
@@ -8,14 +8,13 @@
  * - email
  * - password
  */
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 // const path = require('path');
 const router = require('express').Router();
 let User = require('../module/user_schema');
 // const emailRegExp =
 //   /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 // const pwdRegExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,24}$/;
-
 
 // First end point that handles incoming HTTP GET requests on the users URL path
 router.route('/').get((req, res) => {
@@ -99,22 +98,24 @@ router.route('/').get((req, res) => {
  *
  */
 
+ //Mizuho
 // POST
 router.route('/register').post((req, res) => {
+
   const email = req.body.email;
   const fname = req.body.fname;
   const lname = req.body.lname;
-  const birthday = req.bodybirthday; //fix later
   const password = req.body.password;
   //   const pimage = req.body.pimage;
   //   const introduce = req.body.introduce;
   //   const user_rate = req.body.user_rate;
 
+  //const encryptedPassword = bcrypt.hash(password, 10);
+ 
   const newUser = new User({
     email,
     fname,
     lname,
-    birthday,
     password,
     //   pimage,
     //   create_date,
@@ -122,11 +123,81 @@ router.route('/register').post((req, res) => {
     //   user_rate
   });
 
-  newUser
+  // const existedUser = User.findOne({email});
+  // if(existedUser){
+  //   return res.send({error: "User exists. Please log in."})
+  // }
+
+   newUser
     .save()
     .then(() => res.json('User added!'))
     .catch((err) => res.status(400).json('Error: ' + err));
 });
+
+//Mizuho
+// router.route('/login').get((req, res) => {
+//   res.render('login', { user: req.session.user, layout: false });
+// });
+
+// app.post('/login', function (req, res) {
+//   if (req.body.email === '' || req.body.password === '') {
+//     return res.render('login', {
+//       errorMsg: 'Both user email and password are required,',
+//       user: req.session.user,
+//       layout: false,
+//     });
+//   }
+
+//   User.findOne({ email: req.body.email })
+//     .exec()
+//     .then((user) => {
+//       if (!user) {
+//         // If the email does not exist
+//         res.render('login', {
+//           errorMsg: 'Email does not exist.Please sign up an account',
+//           user: req.session.user,
+//           layout: false,
+//         });
+//       } else {
+//         // If the email exists
+//         bcrypt
+//           .compare(req.body.psw, user.create_psw)
+//           .then((result) => {
+//             if (result) {
+//               // If the passwords are matched
+//               console.log('matched');
+//               req.session.user = {
+//                 email: user.email,
+//                 isAdmin: true, //or user.isAdmin??? Just put {{#if isAdmin}}
+//                 fname: user.fname,
+//                 lname: user.lname,
+//               };
+
+//               if (user.isAdmin) {
+//                 //パスワードがadminのパスワードと一致する場合はAdminDashboardにリダイレクト
+//                 console.log('this account is admin');
+//                 return res.redirect('adminDashboard');
+//               }
+
+//               return res.redirect('/'); //Adminじゃないパスワードの場合はuserDashboardにリダイレクト
+//             } else {
+//               // If the login info does not match
+//               res.render('login', {
+//                 errorMsg: 'login email and password does not match!',
+//                 user: req.session.user,
+//                 layout: false,
+//               });
+//             } //end line 209
+//           }) //end line 190
+//           .catch((err) => {
+//             console.log(`An error occurred: ${err}`);
+//           });
+//       } //end bcrypt.compare
+//     }) //end else
+//     .catch((err) => {
+//       console.log(`Something went wrong: ${err}`);
+//     });
+// }); //end app.post
 
 /*
 router.route('/register').post((req, res) => {
