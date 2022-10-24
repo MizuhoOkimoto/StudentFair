0dd6493659f0bdcf926dd4fc69d851496c51ace6
Mizuho Okimoto <mokimoto@myseneca.ca>
2022-10-24 15:10:32 -0400

    Add a user table for the Admin page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,10 +1,47 @@
-const Admin = (prop) => {
-  
-    return (
-      <div className="admin-container">
-        <h1>This is Admin page</h1>
+import Table from 'react-bootstrap/Table';
+import '../components/css/Admin.css';
+
+const Admin = () => {
+  return (
+    <div className="admin-container">
+        <div className="admin-card">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Phone</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>testname</td>
+            <td>test</td>
+            <td>test</td>
+            <td>@111-111-1111</td>
+            <td>Toronto</td>
+          </tr>
+          <tr>
+            <td>testname</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>456-456-4564</td>
+            <td>Niagara</td>
+          </tr>
+          <tr>
+            <td>testname</td>
+            <td>Jack</td>
+            <td>Thornton</td>
+            <td></td>
+            <td>Niagara</td>
+          </tr>
+        </tbody>
+      </Table>
       </div>
-    );
-  };
-  
-  export default Admin;
+    </div>
+  );
+};
+
+export default Admin;
