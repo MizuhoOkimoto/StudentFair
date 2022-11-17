1c12b1a18c46d17193d1c8e14945e8be51277e70
Mizuho Okimoto <mokimoto@myseneca.ca>
2022-11-17 04:45:57 -0500

    Implemented Contact Us page

--- src/pages/CustomerService.js
+++ src/pages/CustomerService.js
@@ -3,77 +3,61 @@ import styled from 'styled-components';
 import axios from 'axios';
 import '../components/css/LogIn-Register.css';
 import { Link } from 'react-router-dom';
+import { Card, Button } from 'react-bootstrap';
+import customer_service from '../img/contactus_pic/customer_service.png';
+import sales_service from '../img/contactus_pic/sales.png';
+import tech_service from '../img/contactus_pic/tech_service.png';
 
 
 function CustomerService(prop) {
-  const create_date = new Date();
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-
-    const newReport = {
-      email: prop.userData.email,
-      category: '',
-      title: e.target.title.value,
-      description: e.target.desc.value,
-      date: create_date,
-    };
-
-    axios.post('http://localhost:8080/reports/customerService', newReport).then((res) => {
-      console.log(res);
-      if (res.data === true) {
-        alert('Your service ticket successfully submitted');
-        window.location = '/';
-      } else {
-        alert('You need to type details. Please check again.');
-      }
-    }, []);
-  };
 
   return (
-    <div className="signUp-container">
-      <form className="user-form sign-up" action="/" method="POST" onSubmit={onSubmitHandler}>
-        <div className="title">
-          <div className="form-title">
-            <p>Contact CustomerService</p>
-          </div>
-          <i className="fas fa-times"></i>
-        </div>
-        <div className="border" />
-        <div className="input-container">
-          User Email
-          <span>{prop.userData.email}</span>
-        </div>
-        <div className="input-container">
-          User Name
-          <span>
-            {prop.userData.fname} {prop.userData.lname}
-          </span>
-        </div>
-        <div className="input-container">
-          Date
-          <span>
-            {create_date.getFullYear()}-{create_date.getMonth() + 1}-{create_date.getDate()}
-          </span>
-        </div>
-        <div className="input-container">
-          <input type="text" name="title" id="title" placeholder="Title" />
-        </div>
-        <textarea
-          className="report-text-area"
-          type="text"
-          name="desc"
-          id="desc"
-          placeholder="Details"
-          maxLength="1000"
-          cols="88"
-          rows="7"
-        />
+    <div className="contact_us_container" style={{height:"60vh"}} >
+      <h1 style={{color:"#e9967a",fontSize:"4.5rem", marginTop:"80px", fontWeight:"780"}}><span style={{color:"blue", fontWeight:"800", fontSize:"6rem"}}>C</span>ontact Us</h1>
+      <h2 style={{fontSize:"2rem", color:"gray", marginTop:"10px"}}>Have any questions? We'd love to hear from you.</h2>
+      <div className="contact_cards" style={{display:"flex", justifyContent: "center", marginTop: "50px", marginBottom: "50px"}}>
+      
+      <Card style={{ width: '18rem', boxShadow:"0px 1px 1px gray", margin:"20px" }}>
+      <Card.Body style={{position:"relative"}}>
+        <img className="customer_service" src={customer_service} alt="customer_service Img" style={{width:"100px", height:"100px"}}></img>
+        <Card.Title style={{fontSize:"2rem", marginTop:"0px"}}>Customer Service</Card.Title>
+        <Card.Text>
+          Contact Mizuho <br/>
+          E-mail: mokimoto@myseneca.ca
+          Phone: 416-416-4161 (9am-5pm)
+        </Card.Text>
+        <Button variant="primary" onClick={() => window.location = 'mokimoto@myseneca.ca'}>Send an E-mail</Button>
+      </Card.Body>
+    </Card>
 
-        <div className="input-container sign-up">
-          <input type="submit" name="submit" id="submitSignUp" value="Submit" />
-        </div>
-      </form>
+    <Card style={{ width: '18rem', boxShadow:"0px 1px 1px gray", margin:"20px" }}>
+      <Card.Body style={{position:"relative"}}>
+        <img className="sales_service" src={sales_service} alt="sales_service Img" style={{width:"123px", height:"100px"}}></img>
+        <Card.Title style={{fontSize:"2rem", marginTop:"0px"}}>Sales</Card.Title>
+        <Card.Text>
+          Contact Jun <br/>
+          E-mail: jsong89@myseneca.ca
+          Phone: 513-513-5131 (9am-5pm)
+        </Card.Text>
+        <Button variant="primary" onClick={() => window.location = 'jsong89@myseneca.ca'}>Send an E-mail</Button>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem', boxShadow:"0px 1px 1px gray", margin:"20px" }}>
+      <Card.Body style={{position:"relative"}}>
+        <img className="tech_service" src={tech_service} alt="tech_service Img" style={{width:"85px", height:"85px"}}></img>
+        <Card.Title style={{fontSize:"2rem", marginTop:"15px"}}>Technical Service</Card.Title>
+        <Card.Text>
+          Contact Wonchul <br/>
+          E-mail: wchoi28@myseneca.ca
+          Phone: 789-789-7897 (9am-5pm)
+        </Card.Text>
+        <Button variant="primary" onClick={() => window.location = 'wchoi28@myseneca.ca'}>Send an E-mail</Button>
+      </Card.Body>
+    </Card>
+
+    </div>
+
     </div>
   );
 }
